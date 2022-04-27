e4d0c61b23dd1255181ccb796c92afd47f60287a
jipdesign-jp <jipdesigninfo@gmail.com>
2022-04-27 17:53:10 +0900

    timer 1000

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{minutes}:{second}")
     if count > 0:
         global  timer
-        timer = window.after(1, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         mark =""
